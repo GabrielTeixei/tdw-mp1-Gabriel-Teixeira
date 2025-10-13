c15490f6edc726c89bc625b23dc81e0cf050dc34
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-10-13 16:15:13 +0100

    Atualiza ESLint para TypeScript/React e corrige erros de lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,34 @@
 module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020, 
+    ecmaVersion: 2020,
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
     browser: true,
     node: true,
   },
-  rules: {},
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals"
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "EXAMPLE_PATH" }]
+  },
 };
