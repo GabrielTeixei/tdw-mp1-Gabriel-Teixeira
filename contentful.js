206653affa980f61a27652f5f42c5616d1c74a37
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-10-15 11:47:37 +0100

    fix contentful.js

--- contentful.js
+++ contentful.js
@@ -1,6 +1,10 @@
 import { createClient } from 'contentful';
 
+// export const client = createClient({
+//   space: process.env.CONTENTFUL_TOKEN,
+//   accessToken: process.env.PREVIEW_TOKEN,
+// });
 export const client = createClient({
-  space: process.env.CONTENTFUL_TOKEN,
-  accessToken: process.env.PREVIEW_TOKEN,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
