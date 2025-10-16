6d0afd3ef57ed16b0725ff82b44a8e932e69a339
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-10-16 15:40:09 +0100

    Apagar ficheiro

--- lib/contentful.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "contentful";
-
-export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID!,           // mantém o mesmo
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,        // atualizado para o nome do secret
-});
